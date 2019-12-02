5adbbc231bdab5ecbe1268d9c05b58bc501dd7e2
creativeBernadka <bfryczkowska@gmail.com>
2019-12-02 14:39:32 +0100

    REFACTOR: fetch 'accounts' from API in '/settings' route

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default class SettingsRoute extends Route {
   model(){
-    return this.store.findAll('account');
+    return RSVP.hash({
+      accounts: this.store.findAll('account'),
+      categories: this.store.findAll('category')
+    });
   }
 }
