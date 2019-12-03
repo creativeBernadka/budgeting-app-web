19442d582cd3c4ac8f068e8e293843ee81fdeeb4
creativeBernadka <bfryczkowska@gmail.com>
2019-12-03 14:01:13 +0100

    REFACTOR: move mirage data to variables

--- mirage/config.js
+++ mirage/config.js
@@ -1,6 +1,6 @@
 import DS from "ember-data";
 
-const data = [
+const transactions = [
   {
     type: 'transactions',
     id: 1,
@@ -46,126 +46,129 @@ const data = [
     }
   }
 ];
+const accounts = [
+  {
+    type: 'accounts',
+    id: '1',
+    attributes: {
+      "account-name": 'ING',
+      "current-value": 1000,
+      "account-type": 'checking',
+      "interest-rate": 0,
+      "currency": 'PLN'
+    }
+  },
+  {
+    type: 'accounts',
+    id: '2',
+    attributes: {
+      "account-name": 'Cash',
+      "current-value": 70,
+      "account-type": 'checking',
+      "interest-rate": 0,
+      "currency": 'PLN'
+    }
+  },
+  {
+    type: 'accounts',
+    id: '3',
+    attributes: {
+      "account-name": 'ING - OKO',
+      "current-value": 5500,
+      "account-type": 'savings',
+      "interest-rate": 0.7,
+      "currency": 'PLN'
+    }
+  }
+];
+const categories = [
+  {
+    type: 'categories',
+    id: 1,
+    attributes: {
+      "category-name": "food",
+      "category-type": "outcome",
+      "subcategories": ["home", "eating-out"]
+    }
+  },
+  {
+    type: 'categories',
+    id: 2,
+    attributes: {
+      "category-name": "indulgences",
+      "category-type": "outcome",
+      "subcategories": ["cinema"]
+    }
+  },
+  {
+    type: 'categories',
+    id: 3,
+    attributes: {
+      "category-name": "home",
+      "category-type": "outcome",
+      "subcategories": ["furniture etc", "detergents"]
+    }
+  },
+  {
+    type: 'categories',
+    id: 4,
+    attributes: {
+      "category-name": "cosmetics",
+      "category-type": "outcome",
+      "subcategories": []
+    }
+  },
+  {
+    type: 'categories',
+    id: 5,
+    attributes: {
+      "category-name": "presents",
+      "category-type": "outcome",
+      "subcategories": []
+    }
+  },
+  {
+    type: 'categories',
+    id: 6,
+    attributes: {
+      "category-name": "wage",
+      "category-type": "income",
+      "subcategories": []
+    }
+  },
+  {
+    type: 'categories',
+    id: 7,
+    attributes: {
+      "category-name": "return money",
+      "category-type": "income",
+      "subcategories": []
+    }
+  }
+];
+
 export default function() {
   this.namespace = '/api';
 
   this.get('/transactions', function () {
     return {
-      data: data
+      data: transactions
     }
   });
 
   this.post('/transactions', function (schema) {
-    data.push(schema)
+    transactions.push(schema)
   });
 
   this.get('/accounts', function () {
     return {
-     data: [
-       {
-         type: 'accounts',
-         id: '1',
-         attributes: {
-           "account-name": 'ING',
-           "current-value": 1000,
-           "account-type": 'checking',
-           "interest-rate": 0,
-           "currency": 'zł'
-         }
-       },
-       {
-         type: 'accounts',
-         id: '2',
-         attributes: {
-           "account-name": 'Cash',
-           "current-value": 70,
-           "account-type": 'checking',
-           "interest-rate": 0,
-           "currency": 'zł'
-         }
-       },
-       {
-         type: 'accounts',
-         id: '3',
-         attributes: {
-           "account-name": 'ING - OKO',
-           "current-value": 5500,
-           "account-type": 'savings',
-           "interest-rate": 0.7,
-           "currency": 'zł'
-         }
-       }
-     ]
+     data: accounts
     }
   });
 
   this.get('/categories', function () {
     return {
-      data: [
-        {
-          type: 'categories',
-          id: 1,
-          attributes: {
-            "category-name": "food",
-            "category-type": "outcome",
-            "subcategories": ["home", "eating-out"]
-          }
-        },
-        {
-          type: 'categories',
-          id: 2,
-          attributes: {
-            "category-name": "indulgences",
-            "category-type": "outcome",
-            "subcategories": ["cinema"]
-          }
-        },
-        {
-          type: 'categories',
-          id: 3,
-          attributes: {
-            "category-name": "home",
-            "category-type": "outcome",
-            "subcategories": ["furniture etc", "detergents"]
-          }
-        },
-        {
-          type: 'categories',
-          id: 4,
-          attributes: {
-            "category-name": "cosmetics",
-            "category-type": "outcome",
-            "subcategories": []
-          }
-        },
-        {
-          type: 'categories',
-          id: 5,
-          attributes: {
-            "category-name": "presents",
-            "category-type": "outcome",
-            "subcategories": []
-          }
-        },
-        {
-          type: 'categories',
-          id: 6,
-          attributes: {
-            "category-name": "wage",
-            "category-type": "income",
-            "subcategories": []
-          }
-        },
-        {
-          type: 'categories',
-          id: 7,
-          attributes: {
-            "category-name": "return money",
-            "category-type": "income",
-            "subcategories": []
-          }
-        }
-      ]
+      data: categories
     }
   })
 }
