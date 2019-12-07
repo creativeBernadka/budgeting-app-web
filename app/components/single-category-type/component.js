a490e2de6371fa395cbe0342476cbafedfac48fe
creativeBernadka <bfryczkowska@gmail.com>
2019-12-07 19:27:17 +0100

    FEAT: add action to button in single-category-type

--- app/components/single-category-type/component.js
+++ app/components/single-category-type/component.js
@@ -1,4 +1,30 @@
 import Component from '@glimmer/component';
+import {tracked} from "@glimmer/tracking";
+import {action} from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class SingleCategoryTypeComponent extends Component {
+  @service store;
+
+  @tracked showAddField = false;
+  @tracked newCategory = "";
+
+  @action
+  showInput(){
+    this.showAddField = true;
+  }
+
+  @action
+  enterNewCategory(category){
+    this.newCategory = "";
+    this.showAddField = false;
+    const newRecord = this.store.createRecord('category', {
+      categoryName: category,
+      categoryType: this.args.type.toLowerCase(),
+      subcategories: []
+    });
+
+    newRecord.save()
+
+  }
 }
