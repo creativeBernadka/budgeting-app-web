21e4d740e066cd4e8be73a790b0df42d152cd57c
creativeBernadka <bfryczkowska@gmail.com>
2019-12-11 10:21:08 +0100

    REFACTOR: delete setupController from transactions route

--- app/routes/transactions.js
+++ app/routes/transactions.js
@@ -3,8 +3,5 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(){
     return this.store.findAll('transaction');
-  },
-  setupController(controller, model) {
-    controller.set('model', model);
   }
 });
