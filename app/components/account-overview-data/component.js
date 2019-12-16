5e11ba67d3027b4f586b9f60d9f7aec8aa470444
creativeBernadka <bfryczkowska@gmail.com>
2019-12-16 11:40:59 +0100

    FEAT: add components account-overview and account-overview-data
    Move displaying account overview to account-overview.
    Move getting data to account-overview-data.

---
+++ app/components/account-overview-data/component.js
@@ -0,0 +1,34 @@
+import Component from '@glimmer/component';
+import {tracked} from "@glimmer/tracking";
+import {task} from "ember-concurrency";
+import {inject as service} from '@ember/service';
+import {action} from '@ember/object';
+
+export default class AccountOverviewDataComponent extends Component {
+
+  @service store;
+
+  accountId = this.args.accountId;
+
+  @tracked dataInstance;
+
+  @action
+  getData(){
+    this.dataInstance = this.data.perform();
+  }
+
+  @task (function *() {
+    const account = yield this.store.findRecord('account', this.accountId);
+    const chartData = yield this.store.query('chart-datum',
+      {
+        type: "expense",
+        id: this.accountId,
+        stacked: true
+      });
+
+    return {
+      account,
+      chartData
+    }
+  }) data;
+}
--- app/routes/account.js
+++ app/routes/account.js
@@ -4,14 +4,8 @@ import RSVP from 'rsvp'
 export default Route.extend({
   model({account_id}){
     return RSVP.hash({
-      accountData: this.store.findRecord('account', account_id),
-      history: this.store.query('transaction', {id: account_id}),
-      chartData: this.store.query('chart-datum',
-        {
-          type: "expense",
-          id: account_id,
-          stacked: true
-        })
+      accountId: account_id,
+      history: this.store.query('transaction', {id: account_id})
     })
   }
 });
